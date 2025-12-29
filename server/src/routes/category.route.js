d8a4b4ac83e360a1db2610df05c72f5394f69faf
Dawit Worku <dawitworkujima@gmail.com>
2025-12-29 13:54:54 +0300

    feat: Update category routes to restrict access to admin role and improve code formatting

--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -1,18 +1,39 @@
 import express from "express";
 
-import { createCategory, deleteCategory, getCategories, getSingleCategory, updateCategory } from "../controllers/category.controller.js";
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  getSingleCategory,
+  updateCategory,
+} from "../controllers/category.controller.js";
 import upload from "../middlewares/uploadMiddleware.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { authorize } from "../middlewares/roleCheckMiddleware.js";
 
 const router = express.Router();
 
-
-router.post("/create-category",protect, authorize("organizer","admin"), upload.single("image"), createCategory);
+router.post(
+  "/create-category",
+  protect,
+  authorize("admin"),
+  upload.single("image"),
+  createCategory
+);
 router.get("/get-category", protect, getCategories);
 router.get("/get-single-category/:id", protect, getSingleCategory);
-router.put("/update-category/:id", protect, authorize("organizer","admin"), upload.single("image"), updateCategory);
-router.delete("/delete-category/:id",protect, authorize("organizer","admin"), deleteCategory);
+router.put(
+  "/update-category/:id",
+  protect,
+  authorize("admin"),
+  upload.single("image"),
+  updateCategory
+);
+router.delete(
+  "/delete-category/:id",
+  protect,
+  authorize("admin"),
+  deleteCategory
+);
 
-
-export default router
+export default router;
